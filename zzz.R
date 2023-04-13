25956dec06632066c003e8346fdce89edc617002
Candace Savonen <cansav09@gmail.com>
2023-04-13 14:01:47 -0400

    Add files via upload

---
+++ zzz.R
@@ -0,0 +1,19 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Please use predefined Credentials only for the testing requests. To obtain your own Credentials see help(authorize).")
+}
+
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.slides <- list(
+    slides.client.id = "10709400262-28lpv43nui21l1172cup6kh68blvkllq.apps.googleusercontent.com",
+    slides.client.secret = "FADkDoE_H0B7j-VytEjTbgaU",
+    slides.endpoint.create = "https://classroom.googleapis.com/v1/courses",
+    slides.endpoint.get = "https://classroom.googleapis.com/v1/courses/{courseID}",
+    slides.endpoint.batchUpdate = "https://classroom.googleapis.com/v1/courses/{courseID}:batchUpdate",
+    slides.endpoint.page.get = "https://classroom.googleapis.com/v1/courses/{courseID}/pages/{pageObjectId}"
+  )
+  toset <- !(names(op.slides) %in% names(op))
+  if (any(toset)) options(op.slides[toset])
+
+  invisible()
+}
