936ef3687a8716619d050e25380b1c0e3b7e86d0
Candace Savonen <cansav09@gmail.com>
2023-04-14 10:23:32 -0400

    Try different strategy

--- R/auth.R
+++ R/auth.R
@@ -1,42 +1,45 @@
-.classroomEnv <- new.env(parent = emptyenv())
-.classroomEnv$Token <- NULL
+library(googleAuthR)
 
-# Set token to environment
-set_token <- function(value) {
-  .classroomEnv$Token <- value
-  return(value)
+googleAuthR::gar_set_client(".secrets/client_secret_399382401954-f98jfqie6uuvlg5pl6td7i5efn2hi8hu.apps.googleusercontent.com.json",
+                            scopes = c("https://www.googleapis.com/auth/classroom.courses.readonly",
+                                       "https://www.googleapis.com/auth/classroom.topics",
+                                       "https://www.googleapis.com/auth/classroom.topics.readonly",
+                                       "https://www.googleapis.com/auth/classroom.courses"),
+                            )
+auth <- gar_auth()
+
+secrets <- fromJSON(".secrets/client_secret_399382401954-f98jfqie6uuvlg5pl6td7i5efn2hi8hu.apps.googleusercontent.com.json")
+
+config <- httr::config(token=auth$credentials$id_token)
+
+authorize_from_secret <- function(access_token) {
+
+  #client_id <- getOption("classroom.client.id")
+  #client_secret <- getOption("classroom.client.secret")
+
+  auth <- gar_auth()
+
+  app <- httr::oauth_app(appname = "rgoogleclassroom",
+                         key = secrets$installed$client_id,
+                         secret = secrets$installed$client_secret)
+
+  endpoint <- httr::oauth_endpoints("google")
+
+  token <- httr::oauth2.0_token(endpoint = endpoint, app = app,
+                                scope = c("https://www.googleapis.com/auth/classroom.courses.readonly",
+                                          "https://www.googleapis.com/auth/classroom.topics",
+                                          "https://www.googleapis.com/auth/classroom.topics.readonly",
+                                          "https://www.googleapis.com/auth/classroom.courses"),
+                                credentials = auth$credentials)
+  return(out)
 }
 
-# Get token from environment
-get_token <- function() {
-  .classroomEnv$Token
-}
+url <- "https://classroom.googleapis.com/v1/course/NTk0NTA2NTU0Njky"
 
-#' Authorize R package to access Google classroom API
-#' @description This is a function to authorize the R package to access the Googleclassroom API. If no
-#' client.id and client.secret is provided, the package would provide predefined values.
-#' @importFrom httr oauth_app oauth_endpoints oauth2.0_token
-#' @param client_id OAuth client ID. This is obtained from Google API Credentials
-#' @param client_secret OAuth client secret. This is obtained from Google API Credentials
-#' @param token an output from \code{\link{oauth2.0_token}} to set as the
-#' authentication token.
-#' @param ... additional arguments to send to \code{\link{oauth2.0_token}}
-#' @export
-authorize <- function(
-  client_id = getOption("classroom.client.id"),
-  client_secret = getOption("classroom.client.secret"),
-  token = NULL,
-  ...){
-  if (is.null(token)) {
-    app <- oauth_app(appname = "googleclassroom", key = client_id, secret = client_secret)
-    endpoint <- oauth_endpoints("google")
-    token <- oauth2.0_token(endpoint = endpoint, app = app,
-                            scope = c("https://www.googleapis.com/auth/classroom.courses.readonly",
-                                      "https://www.googleapis.com/auth/classroom.topics",
-                                      "https://www.googleapis.com/auth/classroom.topics.readonly",
-                                      "https://www.googleapis.com/auth/classroom.courses"),
-                            ...)
-  }
-  set_token(token)
-  return(invisible(token))
-}
+response <- httr::GET(
+  url,
+  config = token,
+  httr::accept_json()
+)
+
+response
